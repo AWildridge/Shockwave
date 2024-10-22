d93f0a3ff3ad442fc02ff4813293fcf1467dd7fc
EthanColbert8 <emcolbert2@gmail.com>
2024-10-22 10:33:12 -0400

    create initial project structure

---
+++ PrepareSubmission.py
@@ -0,0 +1,3 @@
+import os
+
+#TODO: Write script to prepare zip files for submission to the competition.
---
+++ Train.py
@@ -0,0 +1,9 @@
+import os
+import argparse
+import numpy as np
+import torch
+import torch.nn as nn
+from matplotlib import pyplot as plt
+from itertools import cycle
+
+#TODO: Actually write the training loop, etc.
